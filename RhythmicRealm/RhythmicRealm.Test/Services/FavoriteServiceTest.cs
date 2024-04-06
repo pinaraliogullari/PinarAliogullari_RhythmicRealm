4884dfa8c6f3b539342ab9212a320e7caf19e678
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-04-06 05:50:20 +0300

    test

---
+++ RhythmicRealm/RhythmicRealm.Test/Services/FavoriteServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture;
+using Moq;
+using RhythmicRealm.Data.Abstract;
+using RhythmicRealm.Entity.Concrete.Others;
+using RhythmicRealm.Service.Concrete;
+using Xunit;
+
+namespace RhythmicRealm.Test.Services
+{
+	public class FavoriteServiceTest
+	{
+
+		//private readonly Fixture _fixture;
+  //      public FavoriteServiceTest()
+  //      {
+  //          _fixture = new Fixture();
+  //      }
+
+        [Fact]
+		public async Task GetAllFavoritesAsync_ReturnFailResponse_WhenFavoriteProductNotFound()
+		{
+			var userId = Guid.NewGuid().ToString();
+			var favoriteRepoMock = new Mock<IFavoriteRepository>();
+			favoriteRepoMock.Setup(x => x.GetFavoritesAsync(userId)).ReturnsAsync(new List<Favorite>());
+
+			var favService = new FavoriteService(favoriteRepoMock.Object);
+
+			var result = favService.GetAllFavoritesAsync(userId);
+
+			Assert.False(result.Result.IsSucceesed);
+			Assert.Equal(result.Result.StatusCode, 404);
+		}
+
+		[Fact]
+		public async Task GetAllFavoritesAsync_ReturnSuccessResponse_WhenGetFavoriteProducts()
+		{
+			var userId = Guid.NewGuid().ToString();
+			var favProducts = new List<Favorite>() { new Favorite() };
+			var favoriteRepoMock = new Mock<IFavoriteRepository>();
+			favoriteRepoMock.Setup(x => x.GetFavoritesAsync(userId)).ReturnsAsync(favProducts);
+
+			var favService = new FavoriteService(favoriteRepoMock.Object);
+
+			var result = favService.GetAllFavoritesAsync(userId);
+
+			Assert.True(result.Result.IsSucceesed);
+			Assert.Equal(result.Result.StatusCode, 200);
+		}
+
+	}
+}
